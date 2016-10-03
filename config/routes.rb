156caa525a3d38e74129814f3b0f5f0ba2d0ef3f
cuong <cuongnguyen71195@gmail.com>
2016-10-03 15:07:42 +0700

    six-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'blog/index'
-  root 'blog#index'
-
+	resources :posts
+	get 'posts/index'
+	root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
