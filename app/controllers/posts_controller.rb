50ec2cb4b709e22b94f3e4fdfaac60ded7d66e7e
cuong <cuongnguyen71195@gmail.com>
2016-10-03 23:03:16 +0700

    12-commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,36 @@
 class PostsController < ApplicationController
-     def index
+    def index
    	   @posts=Post.all.order('created_at DESC')
-   end
-   def new	
-   end
-
-   def create
-   	  @post=Post.new(post_params)
-   	  @post.save
-
-   	  redirect_to @post
-   end
-    def show
-    		@post=Post.find(params[:id])
     end
-   private
-      def post_params
-      	params.require(:post).permit(:title, :text)
-           
-      end
+    def new	
+   	@post=Post.new
+    end
+
+    def create
+   	    @post=Post.new(post_params)
+   	    if @post.save
+           redirect_to @post
+   	    else
+   	  	    render 'new'
+   	  	end
+    end
+    def show
+    	@post=Post.find(params[:id])
+    end
+    def edit
+    	@post=Post.find(params[:id])
+    end
+    def update
+    	@post=Post.find(params[:id])
+
+    	if @post.update(params[:post].permit(:title,:text))
+    		redirect_to @post
+    	else
+    		render 'edit'
+    	
+    end
+    private
+        def post_params
+      	     params.require(:post).permit(:title, :text)
+         end
 end
